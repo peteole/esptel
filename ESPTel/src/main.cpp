588a129e100b718c6f9b1d3b9dae36b3d2364f14
Ole Petersen <peteole2707@gmail.com>
2020-12-22 22:53:10 +0100

    Added file server functionality

--- ESPTel/src/main.cpp
+++ ESPTel/src/main.cpp
@@ -1,9 +1,34 @@
-#include <Arduino.h>
+#include <DNSServer.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <FS.h>
 
-void setup() {
-  // put your setup code here, to run once:
+DNSServer dnsServer;
+AsyncWebServer server(80);
+
+void setup()
+{
+	SPIFFS.begin();
+	WiFi.softAP("esp-captive");
+	dnsServer.start(53, "*", WiFi.softAPIP());
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+		request->send_P(200, "text/html", "This is a great content");
+	});
+	server.on("/home.html", HTTP_GET, [](AsyncWebServerRequest *request) {
+		request->send(SPIFFS, "/home.html", "text/html");
+	});
+	server.on("/main.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+		request->send(SPIFFS, "/main.js", "text/javascript");
+	});
+	server.on("/main.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+		request->send(SPIFFS, "/main.css", "text/css");
+	});
+	//more handlers...
+	server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	dnsServer.processNextRequest();
 }
