8e6a2aefad1b068ef27eefd1c7634eaddf7a321b
jannisan <jan.petersen@imail.de>
2021-01-10 19:56:53 +0000

    eine .JS-Datei, Slider geht

--- ESPTel/data/main.js
+++ ESPTel/data/main.js
@@ -1,3 +1,47 @@
+const slider = document.querySelector(".slider");
+const output = document.querySelector(".sliderVal");
+var interval;
+var response;
+var data;
+var alt;
+var altBias = 0;
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+    interval = parseInt(output.innerText);
+}
+class Voice {
+    constructor() {
+        this.configGUI = document.createElement("div")
+        const voices = window.speechSynthesis.getVoices();
+        const selector = document.createElement("select")
+        this.configGUI.appendChild(selector)
+        const uriToVoice = new Map();
+        for (let voice of voices) {
+            const newOption = document.createElement("option");
+            newOption.innerText = voice.lang + " (Name: " + voice.name + "," + voice.localService + ")";
+            newOption.value = voice.voiceURI;
+            uriToVoice.set(voice.voiceURI, voice)
+            selector.appendChild(newOption);
+        }
+        selector.onchange = (event) => {
+            this.voice = uriToVoice.get(selector.value);
+        }
+        selector.onchange();
+    }
+    /**
+     * 
+     * @param {string} text text to speak
+     */
+    speak(text) {
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = text
+        msg.voice = this.voice
+        window.speechSynthesis.speak(msg);
+    }
+}
+
 /**@type {Voice} */
 let voice = null
 function loadVoices() {
@@ -5,27 +49,36 @@ function loadVoices() {
     document.body.appendChild(voice.configGUI);
 }
 
-async function saydata() {
+async function getData() {
+    response = await fetch("/data")
+    data = await response.json()
+}
 
-    let response = await fetch("/data")
-    let data = await response.json()
-
+function saydata() {
+    getData();
     const text = "Temperatur: " + data.temperature + " °C";
     document.getElementById("temperature-text").innerHTML = text;
 
-    const alt = parseInt(44330 * (1.0 - Math.pow(data.pressure / 1013.25, 0.1903)));
-    const text2 = "Höhe: " + alt + " m";
+    alt = parseInt(44330 * (1.0 - Math.pow(data.pressure / 1013.25, 0.1903)));
+    const height = alt - altBias;
+    const text2 = "Höhe: " + height + " m";
     document.getElementById("altitude-text").innerHTML = text2;
 
     if (voice != null) {
-        voice.speak(text2);
+        voice.speak(text + "  " + text2);
     }
 }
-
-function start() {
-    setInterval(saydata, interval * 1000);
+function altreset() {
+    getData();
+    altBias = alt;
 }
 
+
+async function start() {
+    saydata();
+    setTimeout(start, interval * 1000)
+
+}
 
 
 
