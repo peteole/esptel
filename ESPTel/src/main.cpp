09f2c458ba37937552ae0a8edc2b8ccffaed1625
Ole Petersen <peteole2707@gmail.com>
2021-01-24 13:56:34 +0100

    Moved to genericdmp library

--- ESPTel/src/main.cpp
+++ ESPTel/src/main.cpp
@@ -4,32 +4,23 @@
 #include <ESPAsyncWebServer.h>
 #include <FS.h>
 #include <Adafruit_BMP280.h>
-#include "MPU9250.h"
+#include "MPU9259DMP.h"
+#include "dmp.h"
 #include <Wire.h>
+
 DNSServer dnsServer;
 AsyncWebServer server(80);
 
 Adafruit_BMP280 bmp; // use I2C interface
 Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
 Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
-MPU9250 IMU(Wire, 0x68);
-float ax = 0.0;
-float ay = 0.0;
-float az = 0.0;
-float gx = 0.0;
-float gy = 0.0;
-float gz = 0.0;
-float mx = 0.0;
-float my = 0.0;
-float mz = 0.0;
-float pitch = 0.0;
-float bank = 0.0;
-int status;
+MPU9250DMP dmp;
 
 void setup()
 {
 	Serial.begin(9600);
 	Wire.begin(0, 2);
+	dmp.begin();
 	Serial.println(F("BMP280 Sensor event test"));
 
 	if (!bmp.begin(0x76))
@@ -38,26 +29,6 @@ void setup()
 		while (1)
 			delay(10);
 	}
-
-	status = IMU.begin();
-	if (status < 0)
-	{
-		Serial.println("IMU initialization unsuccessful");
-		Serial.println("Check IMU wiring or try cycling power");
-		Serial.print("Status: ");
-		Serial.println(status);
-		while (1)
-		{
-		}
-	}
-	// setting the accelerometer full scale range to +/-8G
-	IMU.setAccelRange(MPU9250::ACCEL_RANGE_8G);
-	// setting the gyroscope full scale range to +/-500 deg/s
-	IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
-	// setting DLPF bandwidth to 20 Hz
-	IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
-	// setting SRD to 19 for a 50 Hz update rate
-	IMU.setSrd(19);
 
 	SPIFFS.begin();
 	WiFi.softAP("ESP");
@@ -70,25 +41,8 @@ void setup()
 		sensors_event_t temp_event, pressure_event;
 		bmp_temp->getEvent(&temp_event);
 		bmp_pressure->getEvent(&pressure_event);
-		// read the sensor
-		IMU.readSensor();
-
-		ax = IMU.getAccelX_mss() - 0.06;
-		ay = IMU.getAccelY_mss() - 0.05;
-		az = IMU.getAccelZ_mss() + 0.435;
-
-		gx = IMU.getGyroX_rads();
-		gy = IMU.getGyroY_rads();
-		gz = IMU.getGyroZ_rads();
-
-		mx = IMU.getMagX_uT();
-		my = IMU.getMagY_uT();
-		mz = IMU.getMagZ_uT();
-
-		pitch = 180 * atan(ax / az) /PI;
-		bank = 180 * atan(ay / az) / PI;
-
-		String JSON = "{\"temperature\":" + String(temp_event.temperature) + ", \"pressure\":" + String(pressure_event.pressure) + ", \"ax\":" + String(ax) + ", \"ay\":" + String(ay) + ", \"az\":" + String(az) + ", \"gx\":" + String(gx) + ", \"gy\":" + String(gy) + ", \"gz\":" + String(gz) + ", \"mx\":" + String(mx) + ", \"my\":" + String(my) + ", \"mz\":" + String(mz) + ", \"pitch\":" + String(pitch) + ", \"bank\":" + String(bank) + "}";
+		dmp.updateReadables();
+		String JSON = "{\"temperature\":" + String(temp_event.temperature) + ", \"pressure\":" + String(pressure_event.pressure) + ", \"ax\":" + String(dmp.accel.x) + ", \"ay\":" + String(dmp.accel.y) + ", \"az\":" + String(dmp.accel.z) + ", \"pitch\":" + String(dmp.pitch) + ", \"bank\":" + String(dmp.bank) + "}";
 		request->send_P(200, "application/json", &JSON[0]);
 	});
 	server.on("/home.html", HTTP_GET, [](AsyncWebServerRequest *request) {
@@ -114,4 +68,8 @@ void setup()
 void loop()
 {
 	dnsServer.processNextRequest();
+	dmp.readGyro();
+	dmp.readAccel();
+	dmp.processRotations();
+	dmp.processAcceleration();
 }
