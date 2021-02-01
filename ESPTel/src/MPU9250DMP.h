aec64a58a2eb3a9dd0f62a5384eeda6969bd253d
jannisan <jan.petersen@imail.de>
2021-02-01 13:55:50 +0000

    acc offset to MPU9250DMP.h

--- ESPTel/src/MPU9250DMP.h
+++ ESPTel/src/MPU9250DMP.h
@@ -6,24 +6,22 @@
 #include "Wire.h"
 #include "MPU9250.h"
 
-
 class MPU9250DMP : public DMP
 {
 public:
-    MPU9250 mpu;
-    MPU9250DMP() : mpu(Wire, 0x68)
-    {
-    }
-    void readGyro() override;
-    void readAccel() override;
-    // begin sensor reading
-    void begin();
-    void calibrateGyro();
+	MPU9250 mpu;
+	MPU9250DMP() : mpu(Wire, 0x68)
+	{
+	}
+	void readGyro() override;
+	void readAccel() override;
+	// begin sensor reading
+	void begin();
+	void calibrateGyro();
 
 private:
-    long lastRead;
+	long lastRead;
 };
-
 
 void MPU9250DMP::readGyro()
 {
@@ -38,9 +36,9 @@ void MPU9250DMP::readGyro()
 
 void MPU9250DMP::readAccel()
 {
-	accel.x = mpu.getAccelX_mss();
-	accel.y = mpu.getAccelY_mss();
-	accel.z = mpu.getAccelZ_mss();
+	accel.x = mpu.getAccelX_mss() - 0.06;
+	accel.y = mpu.getAccelY_mss() - 0.05;
+	accel.z = mpu.getAccelZ_mss() + 0.435;
 }
 void MPU9250DMP::begin()
 {
